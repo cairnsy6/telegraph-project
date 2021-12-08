11c9e5d353312f05806c27f27f903240fc43c111
Kelvin Stephano <kstephano@gmail.com>
2021-12-08 13:45:41 +0000

    added date and url_id to model

--- server-postgresql/api/models/article.js
+++ server-postgresql/api/models/article.js
@@ -1,17 +1,20 @@
-const db = require ('../dbConfig/init');
+const db = require ('../dbConfig');
 
 class Article {
     constructor(data){
-        this.id = data.id
-        this.title = data.title
-        this.name = data.name
-        this.description = data.description
+        this.id = data.id;
+        this.url_id = {path:`articles/${data.url_id}`};
+        this.title = data.title;
+        this.name = data.name;
+        this.archive_date = data.archive_date;
+        this.description = data.description;
     }
 
     static get all() {
         return new Promise (async (resolve, reject) => {
             try {
                 const articlesData = await db.query(`SELECT * FROM articles;`)
+                console.log(articlesData);
                 const articles = articlesData.rows.map(a => new Article(a))
                 resolve(articles);
             } catch (err) {
@@ -20,10 +23,10 @@ class Article {
         });
     }
 
-    static findById (id) {
+    static findByURL_ID (url_id) {
         return new Promise (async (resolve, reject) => {
             try {
-                let articleData = await db.query(`SELECT * FROM articles WHERE id = $1;`, [ id ]);
+                let articleData = await db.query(`SELECT * FROM articles WHERE url_id = $1;`, [ id ]);
                 let article = new Article(articleData.rows[0]);
                 resolve (article);
             } catch (err) {
@@ -32,10 +35,10 @@ class Article {
         });
     } 
 
-    static create(title, name, description){
+    static create(data){
         return new Promise (async (resolve, reject) => {
             try {
-                let articleData = await db.query(`INSERT INTO articles (title, name, description) VALUES ($1, $2, $3) RETURNING *;`, [ title, name, description]);
+                let articleData = await db.query(`INSERT INTO articles (title, name, article_date, description) VALUES ($1, $2, $3, $4) RETURNING *;`, [ data.title, data.name, data.article_date, data.description]);
                 let newArticle = new Article(articleData.rows[0]);
                 resolve (newArticle);
             } catch (err) {
