2b476b33e7e5c5ef319d1f24d349e3b32f6b3df9
Kelvin Stephano <kstephano@gmail.com>
2021-12-08 13:42:56 +0000

    moved files

--- server-postgresql/api/dbConfig/1_setup.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS articles;
-
-CREATE TABLE articles (
-    id serial PRIMARY KEY,
-    title varchar(255) NOT NULL,
-    name varchar(255) NOT NULL,
-    description varchar(1000) NOT NULL
-);
-
