abcac63596f1230bff8271ce109aec259c9799c6
Kelvin Stephano <kstephano@gmail.com>
2021-12-08 13:44:06 +0000

    created upload() func

---
+++ client/static/js/index.js
@@ -0,0 +1,63 @@
+const url = `http://localhost:3000/articles`;
+
+// init html elements as js objects
+const form = document.querySelector('form');
+const titleInput = document.querySelector('#title-input');
+const authorInput = document.querySelector('#author-input');
+const descriptionInput = document.querySelector('#description-input');
+// const submitBtn = document.querySelector('#submit-btn');
+
+initListeners();
+
+function initListeners() {
+    form.addEventListener("submit", upload)
+}
+
+function getDate() {
+    const date = new Date();
+    const month = date.getUTCMonth() + 1;
+    const day = date.getUTCDate();
+    return `${day}-${month}`;
+}
+
+function getUrlId() {
+    const randNum = Math.floor((Math.random() * 100) +1);
+    return `${titleInput.textContent}-${getDate()}-${randNum}`;
+}
+
+/**
+ * Uploads the article to the server.
+ * 
+ */
+async function upload(e) {
+    e.preventDefault();
+    const url_id = getUrlId();
+    const date = getDate();
+
+    const data = {
+        url_id: url_id,
+        title: titleInput.textContent,
+        name: authorInput.textContent,
+        archive_date: date,
+        description: descriptionInput.textContent
+    }
+
+    console.log(data);
+
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        header: {
+            "Content-Type": "application/json"
+        }
+    }
+
+    try {
+        await fetch(`${url}`, options);
+    } catch (err) {
+        console.log(err);
+    }
+    // localStorage.setItem('url_id', url_id);
+    // location.href = "article.html";
+}
+
